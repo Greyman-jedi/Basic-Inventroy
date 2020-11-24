8beaedebaaf37883371732a6295e60579eeb0766
Greyman-jedi <73208094+Greyman-jedi@users.noreply.github.com>
2020-11-24 14:44:50 -0500

    Added get.line
    Tried to make it easier to allow user to input anything they want using the get.line.

--- main.cpp
+++ main.cpp
@@ -1,16 +1,19 @@
 #include <iostream>
 #include <string>
+#include <cstring>
 
 using namespace std;
 
 int main()
 {
     int a;
-    string item1;
+    char item [10];
     
     cout << "Please add the names of your inventory." << endl;
-    cin >> item1;
-    cout << "Item is: "<<  item1 << endl;
+    cin.getline (item, 10);
+    cout << "Enter the how many"<< endl;
+    cin >> a;
+    cout << "Item is: "<<  item << ", "<< a << " are in inventory."<< endl;
 
     return 0;
 }
