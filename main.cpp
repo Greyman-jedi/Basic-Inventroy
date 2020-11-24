e67c9a2cc1095ee01631671e912ead78d5e5630d
Greyman-jedi <73208094+Greyman-jedi@users.noreply.github.com>
2020-11-24 14:22:13 -0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    int a;
+    string item1;
+    
+    
+    cout << "Please add the names of your inventory." << endl;
+    cin >> item1;
+    cout << "Item is: "<<  item1 << endl;
+
+    return 0;
+}
